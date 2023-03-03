7d46f488d0c20567766fe8a3b78eba48bfc0d3e5
alphahyper <85323549+alphahyper@users.noreply.github.com>
2023-03-03 16:25:49 -0600

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -6,19 +6,19 @@
 #changes directory to backups
 cd ~/Minecraft
 
-#File Name Generator:
+#Date Generator
 #finds date and sets it as a variable
 day=$(date +"%m.%d.%y")
+
+
+#Daily Backups: Creates daily backups
 #finds and adds the number of daily backups made (counter)
 dailyCounter=$(cat backups/daily/counter)
 let dailyCounter++
-#updates counter file
+#updates daily counter file
 echo $dailyCounter > backups/daily/counter
-#uses temp variable for formating of file name (this variable will format all file names)
+#uses temp variable for formating of file name (this variable will format all backup file names)
 tempFormat=$(printf "%05d" $dailyCounter)
-
-
-#Daily Backups: Creates backups everyday
 #creates daily backup folder (counter-d.m.y)
 mkdir backups/daily/$tempFormat'-'$day
 #backups data into new folder
@@ -33,14 +33,41 @@ deleteFormat=$(printf "%05d" $dailyDeletion)
 rm -r backups/daily/$deleteFormat*
 
 
-#Weekly Backups: Creates backups every Monday
+#Weekly Backups and Deletion: Creates backups and deletes every Monday
+
 #checks if it is Monday
+if [ $(date +"%a") = Mon ]; then
 
-#Weekly Deletion: Deletes weekly backups from 4 weeks ago
-#if true, backups into weekly folder & deletes the backup 4 weeks ago
-
+  
+  #Weekly Backups: Creates weekly backups
+  ##finds and adds the number of weekly backups made (counter)
+  weeklyCounter=$(cat backup/weekly/counter)
+  let weeklyCounter++
+  #updates weekly counter file
+  echo $weeklyCounter > backups/weekly/counter
+  #uses temp variable for formating of file name (this variable will format all backup file names)
+  tempFormat=$(printf "%05d" $weeklyCounter)
+  #creates daily backup folder (counter-d.m.y)
+  mkdir backups/weekly/$tempFormat'-'$day
+  #backups data into new folder
+  cp -r world world_nether world_the_end backups/weekly/$tempFormat'-'$day
+  
+  #Weekly Deletion: Deletes weekly backups from 4 weeks ago
+  #creates a variable of the counter 4 weeks ago
+  weeklyDeletion=$((weeklyCounter - 4))
+  #uses temp variable for formating of file name (this variable will format all deletion file names)
+  deleteFormat=$(printf "%05d" $weeklyDeletion)
+  #deletes the backup
+  rm -r backups/daily/$deleteFormat*
+  
 
 #Monthly Backups: Creates backups every 1st of a month
+
 #checks if it is the 1st of a month
+if [ $(date +"%d") = 1 ]; then
+
+
+
+
 
 #if true, backups into monthly folder
