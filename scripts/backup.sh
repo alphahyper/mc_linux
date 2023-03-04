7a08afb90779291e30bf2cbb44941897052e1bac
alphahyper <ccody2008@gmail.com>
2023-03-04 00:54:30 -0600

    mc_linux

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -11,6 +11,8 @@ cd ~/Minecraft
 day=$(date +"%m.%d.%y")
 
 
+#Daily Backups and Deletion: Creates and deletes backups everyday
+
 #Daily Backups: Creates daily backups
 #finds and adds the number of daily backups made (counter)
 dailyCounter=$(cat backups/daily/counter)
@@ -33,12 +35,11 @@ deleteFormat=$(printf "%05d" $dailyDeletion)
 rm -r backups/daily/$deleteFormat*
 
 
-#Weekly Backups and Deletion: Creates backups and deletes every Monday
+#Weekly Backups and Deletion: Creates and deletes backups every Monday
 
 #checks if it is Monday
 if [ $(date +"%a") = Mon ]; then
 
-  
   #Weekly Backups: Creates weekly backups
   ##finds and adds the number of weekly backups made (counter)
   weeklyCounter=$(cat backup/weekly/counter)
@@ -47,27 +48,34 @@ if [ $(date +"%a") = Mon ]; then
   echo $weeklyCounter > backups/weekly/counter
   #uses temp variable for formating of file name (this variable will format all backup file names)
   tempFormat=$(printf "%05d" $weeklyCounter)
-  #creates daily backup folder (counter-d.m.y)
+  #creates weekly backup folder (counter-d.m.y)
   mkdir backups/weekly/$tempFormat'-'$day
   #backups data into new folder
   cp -r world world_nether world_the_end backups/weekly/$tempFormat'-'$day
-  
+
   #Weekly Deletion: Deletes weekly backups from 4 weeks ago
   #creates a variable of the counter 4 weeks ago
   weeklyDeletion=$((weeklyCounter - 4))
   #uses temp variable for formating of file name (this variable will format all deletion file names)
   deleteFormat=$(printf "%05d" $weeklyDeletion)
   #deletes the backup
-  rm -r backups/daily/$deleteFormat*
-  
+  rm -r backups/weekly/$deleteFormat*
+fi
 
 #Monthly Backups: Creates backups every 1st of a month
 
 #checks if it is the 1st of a month
 if [ $(date +"%d") = 1 ]; then
 
-
-
-
-
-#if true, backups into monthly folder
+  ##finds and adds the number of monthly backups made (counter)
+  monthlyCounter=$(cat backup/monthly/counter)
+  let monthlyCounter++
+  #updates monthly counter file
+  echo $monthlyCounter > backups/monthly/counter
+  #uses temp variable for formating of file name (this variable will format all backup file names)
+  tempFormat=$(printf "%05d" $monthlyCounter)
+  #creates monthly backup folder (counter-d.m.y)
+  mkdir backups/monthly/$tempFormat'-'$day
+  #backups data into new folder
+  cp -r world world_nether world_the_end backups/monthly/$tempFormat'-'$day
+fi
