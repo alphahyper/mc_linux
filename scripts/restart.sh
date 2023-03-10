36586a9803343f916effdb319395c9b4087d0368
alphahyper <85323549+alphahyper@users.noreply.github.com>
2023-03-10 12:32:59 -0600

    Update restart.sh

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -4,25 +4,25 @@
 #(screen -S Minecraft -X stuff) makes everything in quotes execute in console screen (Also ^M = ENTER)
 
 #60 second reminder
-screen -S Minecraft -X stuff "echo say restarting in 60 seconds^M"
+screen -S Minecraft -X stuff "say restarting in 60 seconds^M"
 sleep 50
 #10 second reminder
-screen -S Minecraft -X stuff "echo say restarting in 10 seconds"
+screen -S Minecraft -X stuff "say restarting in 10 seconds^M"
 sleep 5
 #5 second countdown
-screen -S Minecraft -X stuff "echo say restarting in:^M"
-screen -S Minecraft -X stuff "echo say 5^M"
+screen -S Minecraft -X stuff "say restarting in:^M"
+screen -S Minecraft -X stuff "say 5^M"
 sleep 1
-screen -S Minecraft -X stuff "echo say 4^M"
+screen -S Minecraft -X stuff "say 4^M"
 sleep 1
-screen -S Minecraft -X stuff "echo say 3^M"
+screen -S Minecraft -X stuff "say 3^M"
 sleep 1
-screen -S Minecraft -X stuff "echo say 2^M"
-sleep 1
-screen -S Minecraft -X stuff "echo say 1^M"
+screen -S Minecraft -X stuff "say 2^M"
+sleep 1 
+screen -S Minecraft -X stuff "say 1^M"
 sleep 1
 #stops the server
-screen -S Minecraft -X stuff "echo stop^M"
+screen -S Minecraft -X stuff "stop^M"
 
 #backups server
 
