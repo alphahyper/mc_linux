e4c117a81ba48e5034f42267e45ad5391f81f13c
alphahyper <85323549+alphahyper@users.noreply.github.com>
2023-06-10 23:16:20 -0500

    Update restart.sh

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -115,6 +115,13 @@ fi
 
 #Part 3: find latest version or paper
 
+#This script finds the build version and number
+
+#finds/set a variable for the latest version (use once 1.19 comes out)
+latest_version=$(curl https://api.papermc.io/v2/projects/paper | sed 's/.*,"//' | sed 's/"]}//')
+
+#finds/set a variable for the latest build
+latest_build=$(curl https://api.papermc.io/v2/projects/paper/versions/${latest_version} | sed 's/.*,//' | sed 's/]}//')
 
 
 
